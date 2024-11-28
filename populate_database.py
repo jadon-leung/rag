647b4c591ce6bb8b793995bef4a512a21924ed99
jadon-leung <146619444+jadon-leung@users.noreply.github.com>
2024-11-27 23:46:23 -0800

    cleaned up code and comments

--- populate_database.py
+++ populate_database.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import shutil
-# from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
@@ -15,7 +14,6 @@ DATA_PATH = "data"
 
 def main():
 
-    # Check if the database should be cleared (using the --clear flag).
     parser = argparse.ArgumentParser()
     parser.add_argument("--reset", action="store_true", help="Reset the database.")
     args = parser.parse_args()
@@ -43,34 +41,6 @@ def split_documents(documents: list[Document]):
     )
     return text_splitter.split_documents(documents)
 
-
-# def add_to_chroma(chunks: list[Document]):
-#     # Load the existing database.
-#     db = Chroma(
-#         persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
-#     )
-
-#     # Calculate Page IDs.
-#     chunks_with_ids = calculate_chunk_ids(chunks)
-
-#     # Add or Update the documents.
-#     existing_items = db.get(include=[])  # IDs are always included by default
-#     existing_ids = set(existing_items["ids"])
-#     print(f"Number of existing documents in DB: {len(existing_ids)}")
-
-#     # Only add documents that don't exist in the DB.
-#     new_chunks = []
-#     for chunk in chunks_with_ids:
-#         if chunk.metadata["id"] not in existing_ids:
-#             new_chunks.append(chunk)
-
-#     if len(new_chunks):
-#         print(f"👉 Adding new documents: {len(new_chunks)}")
-#         new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
-#         db.add_documents(new_chunks, ids=new_chunk_ids)
-#         db.persist()
-#     else:
-#         print("✅ No new documents to add")
 
 
 def add_to_chroma(chunks: list[Document]):
@@ -101,10 +71,7 @@ def add_to_chroma(chunks: list[Document]):
         else:
             print("No new documents to add")
 
-        # Persist the database
-        # print("Persisting database...")
-        # db.persist()
-        # print("Database persisted successfully!")
+       
 
     except Exception as e:
         print(f"Error in add_to_chroma: {e}")
@@ -123,7 +90,6 @@ def calculate_chunk_ids(chunks):
         page = chunk.metadata.get("page")
         current_page_id = f"{source}:{page}"
 
-        # If the page ID is the same as the last one, increment the index.
         if current_page_id == last_page_id:
             current_chunk_index += 1
         else:
